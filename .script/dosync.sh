c68e7600db6cab81b39dbc373fe29aa1e32c0d1c
Karlatemp <karlatemp@vip.qq.com>
2021-02-02 15:26:48 +0800

    Update dosync.sh

--- .script/dosync.sh
+++ .script/dosync.sh
@@ -2,6 +2,7 @@
 
 git config --local user.name "RepoSync"
 git config --local user.email "RepoSync@localhost"
+git config --local pull.rebase "false"
 
 chmod 400 ./.script/id_rsa
 git config --local core.sshCommand "ssh -i ./.script/id_rsa -F /dev/null -v"
@@ -14,6 +15,9 @@ echo Syncing repos...
 git pull "$source" master
 git pull "$target" master
 
+git pull "$source" master
+git pull "$target" master
+
 echo Updating repos...
 
 git push "$source" master
@@ -22,3 +26,4 @@ git push "$target" master
 git config --local --unset core.sshCommand
 git config --local --unset user.name
 git config --local --unset user.email
+git config --local --unset pull.rebase
