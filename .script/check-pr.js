4925c5d3c132eac261bb299cdd0728d8cbbc44f3
Karlatemp <kar@kasukusakura.com>
2022-07-31 23:12:04 +0800

    workflow update

--- .script/check-pr.js
+++ .script/check-pr.js
@@ -20,6 +20,8 @@ async function main() {
         return process.env.GH_TOKEN;
     })();
 
+    let all_failures = [];
+
     function inDomain(thiz, target) {
         thiz = thiz.replace(/\//g, '.');
         console.log("[inDomain] Checking " + thiz + " is in " + target);
@@ -67,51 +69,81 @@ async function main() {
     console.log("Changed list: " + nameChanged);
     for (const line of nameChanged.split('\n')) {
         if (line[0] === '.') {
-            await fireError("Modifying a hidden directory");
-            return
+            all_failures.push("Modified a entry starts with '.': `" + line + "`")
+        }
+        if (line.indexOf('/') === -1) {
+            // contains top-level file edit
+            all_failures.push("Modified top-level file: `" + line + '`');
         }
         for (let protectedDomain of config.protectedDomains) {
             if (inDomain(line, protectedDomain)) {
-                await fireError("Modifying protected domain: " + protectedDomain);
-                return
+                all_failures.push("Protected domain: `" + protectedDomain + '`: `' + line + '`');
             }
         }
     }
-    for (const line of nameChanged.split('\n')) {
-        if (line.indexOf('/') === -1) {
-            // contains top-level file edit
-            await fireError("Contains top level file: " + line, COMMENT);
-            return;
-        }
-    }
-
     let noDomainFiles = [];
     for (let file of nameChanged.split('\n')) {
         let dom = file.replace('/', '.');
         let hasDomain = false;
+        let passed = false;
+        let matchedDomain = '';
         for (let domain in config.domainOwners) {
             if (inDomain(dom, domain)) {
                 hasDomain = true;
-                let isAllowed = false;
+                if (domain.length > matchedDomain.length) {
+                    matchedDomain = domain
+                }
                 for (let allowed of config.domainOwners[domain]) {
                     if (allowed.toLowerCase() === actor.toLowerCase()) {
-                        isAllowed = true;
+                        passed = true;
                         break;
                     }
-                }
-                if (!isAllowed) {
-                    await fireError("Modifying domain " + domain + " but no permission");
-                    return
                 }
             }
         }
         if (!hasDomain) {
             noDomainFiles.push(file);
+        } else if (!passed) {
+            all_failures.push("No permission to modify domain `" + matchedDomain + '`: `' + file + '`')
         }
     }
 
     if (noDomainFiles.length !== 0) {
-        await fireError("Contains files with no domain: \n\n" + noDomainFiles.join("\n"));
+        all_failures.push("Modifying dummy files:\n\n- `" + noDomainFiles.join("`\n- `") + '`')
+    }
+
+    // noinspection EqualityComparisonWithCoercionJS
+    if (all_failures.length != 0) {
+        console.log(all_failures);
+
+        await fireError(""
+            + "Automatic checking failed:\n\n" + all_failures.join("\n\n") + "\n\n ---------------\n\n"
+            + "当一切无误后，请将此 PR 转为 `Draft` 再标记回 `Ready for review`\n\n"
+            + "> 将 PR 标记为 Draft 的方法: PR 页右上角有一行 `Convert to draft`"
+        )
+        // https://api.github.com/repos/OWNER/REPO/pulls/PULL_NUMBER/requested_reviewers
+
+
+        let requestReviewRequest = require('https').request({
+            host: 'api.github.com',
+            port: 443,
+            path: `/repos/${process.env.REPO_N}/pulls/${process.env.PR_NUM}/requested_reviewers`,
+            method: 'POST',
+            headers: {
+                'User-Agent': 'NodeJs/10',
+                'Content-Type': 'application/vnd.github.v3+json',
+                'Authorization': `token ${RW_TOKEN}`,
+            }
+        }, rsp => {
+            rsp.on('data', d => {
+                process.stdout.write(d);
+            });
+        });
+        requestReviewRequest.write(JSON.stringify({
+            // "reviewers": ["octocat", "hubot", "other_user"],
+            "team_reviewers": ["mirai-repo-review"]
+        }));
+        requestReviewRequest.end();
         return
     }
 
---
+++ .script/ci-setup.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+git fetch origin pull/$PR_NUM/head:THE_PR_PR0
+echo $BASE_SHA
+
+CURRENT_B=$(git branch --show-current)
+echo $CURRENT_B
+
+git branch THE_BASE "$CURRENT_B"
+git branch THE_PR THE_PR_PR0
+
+echo "============"
+git branch
+echo "============"
+
+echo "W1:"
+git merge-base $BASE_SHA THE_PR
+echo "W5:"
+git merge-base THE_BASE THE_PR
+echo "Wawe:"
+git merge-base -a THE_BASE THE_PR
+echo "Wjaiopdja:"
+git merge-base --octopus THE_BASE THE_PR
+echo "aeriawef:"
+git merge-base -a --octopus THE_BASE THE_PR
+echo "END"
+
+#FORK_POINT=$(git merge-base --octopus THE_BASE THE_PR)
+## TODO: git merge-base 上面几条命令都没有输出. 待有缘人修复
+FORK_POINT="THE_BASE"
+echo $FORK_POINT
+
+git rev-list --count "$FORK_POINT..THE_PR" > tmp/count
+cat tmp/count
+git --no-pager diff "$FORK_POINT..THE_PR" --no-color --output tmp/change-diff
+git --no-pager diff "$FORK_POINT..THE_PR" --name-only --output tmp/name-changed
