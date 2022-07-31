7e446632479aa80a113b6d185552472dda3b4961
Karlatemp <kar@kasukusakura.com>
2022-08-01 00:06:26 +0800

    workflow update

--- .script/ci-setup.sh
+++ .script/ci-setup.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env sh
 
-git fetch origin pull/$PR_NUM/head:THE_PR
+git fetch origin pull/$PR_NUM/head:THE_PR_PR0
 echo $BASE_SHA
 git branch
 
 CURRENT_B=$(git branch --show-current)
 echo $CURRENT_B
 
+git branch $BASE_SHA THE_BASE
+git branch THE_PR THE_PR_PR0
+
 echo "W1:"
 git merge-base $BASE_SHA THE_PR
+echo "W5:"
+git merge-base THE_BASE THE_PR
+echo "W6:"
+git merge-base --fork-point THE_BASE THE_PR
 echo "W2:"
 git merge-base --fork-point "$CURRENT_B" THE_PR
 echo "END"
